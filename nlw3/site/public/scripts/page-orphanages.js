0425d84f613e3fe264443e51de1ff232ac7df9cc
Tiago Richter <t2engenharia@gmail.com>
2020-10-15 15:02:34 -0300

    Inicio da aula de back-end. Alteração dos diretórios. Inicialização do servidor.

--- nlw3/site/public/scripts/page-orphanages.js
+++ nlw3/site/public/scripts/page-orphanages.js
@@ -1,22 +1,38 @@
+const { orphanages } = require("../../src/pages");
+
 const map = L.map('mapid').setView([-26.1100481,-49.8046244], 15);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 /* Criar icone */
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: ".//images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 })
 
-// create popup
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+function addMarker({id, name, lat, lng}) {
 
-L.marker([-26.1100481,-49.8046244], { icon }).addTo(map)
-    .bindPopup(popup)
+    // create popup
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"> <img src=".//images/arrow-white.svg"> </a>`)
+
+    L.marker([lat,lng], { icon }).addTo(map)
+        .bindPopup(popup)
+}
+
+const orphanagesSpan= document.querySelectorAll('.orphanages span')
+orphanageSpanforEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+    addMarker(orphanage)
+});
--- nlw3/site/src/server.js
+++ nlw3/site/src/server.js
@@ -1,1 +1,21 @@
-console.log('Olá mundo cruel')
+//importar dependencias
+const express = require('express');
+const path = require('path')
+const pages = require('./pages.js')
+
+//iniciar express
+const server = express();
+server.use(express.static('public'))
+
+//configurar template engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+//rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+//ligar o servidor
+server.listen(5500)
